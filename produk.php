a740bb0b524a24a6a320de060b36ebf5fe1b078b
Nurhaslinda <nrhaslinda29@gmail.com>
2023-06-22 23:38:36 +0700

    Index dan Produk

--- produk.php
+++ produk.php
@@ -1,152 +1,291 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Halaman Produk</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="shortcut icon" href="assets/img/LOGO.png">
+<html>
+   <head>
+      <!-- Basic -->
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <!-- Mobile Metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <!-- Site Metas -->
+      <meta name="keywords" content="" />
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+      <title>Produk</title>
+      <!-- bootstrap core css -->
+      <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" />
+      <!-- font awesome style -->
+      <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+      <!-- Custom styles for this template -->
+      <link href="assets/css/style.css" rel="stylesheet" />
+      <!-- responsive style -->
+      <link href="assets/css/responsive.css" rel="stylesheet" />
+      <link rel="shortcut icon" href="assets/img/LOGO.png">
+   </head>
+   <body>
+      <div class="hero_area">
+         <!-- header section strats -->
+         <header class="header_section">
+            <div class="container">
+               <nav class="navbar navbar-expand-lg custom_nav-container "> 
+                  <a class="navbar-brand" href="index.php"><img width="50" src="assets/img/LOGO.png" alt="#" /></a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class=""> </span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul class="navbar-nav">
+                        <li class="nav-item">
+                           <a class="nav-link" href="index.php">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="#produk">Produk</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="index.php">Tentang Kami</a>
+                        </li>
+                     </ul>
+                  </div>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul class="navbar-nav">
+                        <li class="nav-item">
+                           <button class="nav-link btn btn-warning" type="button" onclick="location.href='#'">Login</button>
+                        </li>
+                        <li class="nav-item" style="margin-left: 10px;">
+                           <a class="nav-link" href="#">Daftar</a>
+                        </li>
+                  </ul>
+                  </div>
+               </nav>
+            </div>
+         </header>
+         <!-- end header section -->
 
-</head>
-<body>
-  <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary py-3">
-      <a class="navbar-brand" href="#">
-        <img src="assets/img/LOGO.png" alt="Logo" width="28" height="33">
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav mx-auto">
-                <li class="nav-item active">
-                <a class="nav-link" href="#">Beranda</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Produk</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Tentang Kami</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Kontak</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <button class="nav-link btn btn-warning" type="button" onclick="location.href='#'">Login</button>
-                </li>
-                <li class="nav-item" style="margin-left: 10px;">
-                    <a class="nav-link" href="#">Daftar</a>
-                </li>
-            </ul>
-        </div>
-
-       
-    </nav>
-
-    <style>
-        .banner{
+         <!-- CSS benner -->
+         <style>
+         .banner{
             background-image: url("assets/img/banner.jpeg");
             background-size: cover;
             background-position: center;
             text-align: center;
             padding: 100px 0;
             color: #fff;
-        }
+         }
+         </style>
+         <!-- End CSS benner -->
 
-        .banner h2 {
-            font-size: 32px;
-            font-weight: bold;
-            margin-bottom: 20px;
-        }
-    </style>
-    <div class="banner">
-        <h2>Web Galeri Produk MI</h2>
-    </div>
+         <!-- Benner -->
+         <div class="banner">
+                 <h2>Galeri Produk</h2>
+          </div>  
+         <!-- Benner -->
 
-    <div class="container bg-primary col-md-12 py-2">
-    <div class="row">
-      <div class="col text-center">
-       <h1>tes</h1>
+          <!-- inner page section -->
+          <section class="inner_page_head">
+                <div class="container_fuild">
+                    <div class="row">
+                    <div class="col-md-12">
+                        <div class="full">
+                            <!-- <h3>Product Grid</h3> -->
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </section>
+      <!-- end inner page section -->
+
+      <!-- product section -->
+      <section class="product_section layout_padding" id="produk">
+         <div class="container">
+            <div class="heading_container heading_center">
+               <h5>Produk MI</h5>
+            </div>
+
+            <!-- Baris 1 -->
+            <div class="row">
+               <div class="col-sm-6 col-md-4 col-lg-3">
+                  <div class="box">
+                     <div class="img-box">
+                        <img src="assets/img/p1.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Mango (Belajar Bahasa Jepang)
+                        </h5>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-3">
+                  <div class="box">
+                     <div class="img-box">
+                        <img src="assets/img/p2.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Al Qur'an Apps (Aplikasi Al Qur'an)
+                        </h5>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-3">
+                  <div class="box">
+                     <div class="img-box">
+                        <img src="assets/img/p3.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Ringkas-Net (Curahkan Tulisanmu)
+                        </h5>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-3">
+                  <div class="box">
+                     <div class="img-box">
+                        <img src="assets/img/p3.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Ringkas-Net (Curahkan Tulisanmu)
+                        </h5>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+            <!-- Baris 2 -->
+            <div class="row">
+               <div class="col-sm-6 col-md-4 col-lg-3">
+                  <div class="box">
+                     <div class="img-box">
+                        <img src="assets/img/p1.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Mango (Belajar Bahasa Jepang)
+                        </h5>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-3">
+                  <div class="box">
+                     <div class="img-box">
+                        <img src="assets/img/p2.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Al Qur'an Apps (Aplikasi Al Qur'an)
+                        </h5>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-3">
+                  <div class="box">
+                     <div class="img-box">
+                        <img src="assets/img/p3.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Ringkas-Net (Curahkan Tulisanmu)
+                        </h5>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-3">
+                  <div class="box">
+                     <div class="img-box">
+                        <img src="assets/img/p3.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Ringkas-Net (Curahkan Tulisanmu)
+                        </h5>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+            <!-- Baris 3 -->
+            <div class="row">
+               <div class="col-sm-6 col-md-4 col-lg-3">
+                  <div class="box">
+                     <div class="img-box">
+                        <img src="assets/img/p1.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Mango (Belajar Bahasa Jepang)
+                        </h5>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-3">
+                  <div class="box">
+                     <div class="img-box">
+                        <img src="assets/img/p2.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Al Qur'an Apps (Aplikasi Al Qur'an)
+                        </h5>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-3">
+                  <div class="box">
+                     <div class="img-box">
+                        <img src="assets/img/p3.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Ringkas-Net (Curahkan Tulisanmu)
+                        </h5>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-3">
+                  <div class="box">
+                     <div class="img-box">
+                        <img src="assets/img/p3.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Ringkas-Net (Curahkan Tulisanmu)
+                        </h5>
+                     </div>
+                  </div>
+               </div>
+            </div>
       </div>
-    </div>
-  </div>
 
-<section id="products">
-  <div class="container">
-    <h2>Galeri Produk</h2>
-    <div class="row">
-      <?php
-      // Dummy data
-      $products = [
-        ["image" => "https://dummyimage.com/300x200/000/fff", "name" => "Product 1", "description" => "Product 1 description"],
-        ["image" => "https://dummyimage.com/300x200/000/fff", "name" => "Product 2", "description" => "Product 2 description"],
-        ["image" => "https://dummyimage.com/300x200/000/fff", "name" => "Product 3", "description" => "Product 3 description"],
-        ["image" => "https://dummyimage.com/300x200/000/fff", "name" => "Product 4", "description" => "Product 4 description"],
-        ["image" => "https://dummyimage.com/300x200/000/fff", "name" => "Product 5", "description" => "Product 5 description"],
-        ["image" => "https://dummyimage.com/300x200/000/fff", "name" => "Product 6", "description" => "Product 6 description"],
-        ["image" => "https://dummyimage.com/300x200/000/fff", "name" => "Product 7", "description" => "Product 7 description"],
-        ["image" => "https://dummyimage.com/300x200/000/fff", "name" => "Product 8", "description" => "Product 8 description"],
-        ["image" => "https://dummyimage.com/300x200/000/fff", "name" => "Product 9", "description" => "Product 9 description"],
-        ["image" => "https://dummyimage.com/300x200/000/fff", "name" => "Product 10", "description" => "Product 10 description"],
-        ["image" => "https://dummyimage.com/300x200/000/fff", "name" => "Product 11", "description" => "Product 11 description"],
-        ["image" => "https://dummyimage.com/300x200/000/fff", "name" => "Product 12", "description" => "Product 12 description"],
-      ];
+            <!-- Pagination -->
+            <div class="pagination-box">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination justify-content-center">
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                        <a class="page-link" href="#">Next</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+         </div>
+      </section>
+      <!-- end product section -->
 
-      foreach ($products as $key => $product) {
-        echo '<div class="col-md-3 mb-4">';
-        echo '<a href="detail_produk.php?id=' . $key . '">';
-        echo '<div class="card">';
-        echo '<img src="' . $product["image"] . '" class="card-img-top" alt="Product Image">';
-        echo '<div class="card-body">';
-        echo '<h3 class="card-title">' . $product["name"] . '</h3>';
-        echo '</div>';
-        echo '</div>';
-        echo '</a>';
-        echo '</div>';
-      }
-      ?>
-    </div>
-  </div>
-
-
-  <?php
-    // $query = "SELECT * FROM products";
-    // $result = mysqli_query($connection, $query);
-
-    // if (mysqli_num_rows($result) > 0) {
-    //   while ($row = mysqli_fetch_assoc($result)) {
-    //     echo "<div class='product'>";
-    //     echo "<img src='images/" . $row['image'] . "' alt='Product Image'>";
-    //     echo "<h3>" . $row['name'] . "</h3>";
-    //     echo "<p>" . $row['description'] . "</p>";
-    //     echo "<span>" . $row['price'] . "</span>";
-    //     echo "</div>";
-    //   }
-    // } else {
-    //   echo "No products found.";
-    // }
-
-    // mysqli_close($connection);
-    ?>
-    
-</section>
-
-  <footer class="bg-primary py-3">
-  <div class="container">
-    <div class="row">
-      <div class="col text-center">
-        <p>&copy; 2023 Web Galeri Produk MI. All rights reserved.</p>
+      <!-- Footer -->
+      <div class="cpy_">
+         <p class="mx-auto">
+            Copyright © All Rights Reserved
+         </p>
       </div>
-    </div>
-  </div>
-</footer>
+      <!-- End Footer -->
 
-
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      
+   </body>
 </html>
